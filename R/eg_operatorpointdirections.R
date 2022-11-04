f777d9a8c733683f8a15dcf3045ef5fcbcf46cd6
Kenneth Rose <kennethrose82@gmail.com>
2022-11-04 10:14:57 +0100

    I've added hasData to the url param.

--- R/eg_operatorpointdirections.R
+++ R/eg_operatorpointdirections.R
@@ -1,10 +1,20 @@
 
 #' Get operator point directions data.
 #'
+#' This list contains all known operator point directions, including the ones involving non-Transmission
+#' System operators; i.e. this list can contain also storage system operators, or LNG system operators, all of
+#' which do not in any way send data to ENTSOG’s Transparency Platform.
+#'
+#' @param has_data To filter the list only those operator point directions
+#'     which belong to a TSO, which itself is publishing
+#'     REG715 information, the parameter hasData can be used.
+#'
 #' @export
-eg_operatorpointdirection <- function(){
+eg_operatorpointdirection <- function(has_data = FALSE){
 
   eg_url <- "https://transparency.entsog.eu/api/v1/operatorpointdirections.json?limit=-1"
+
+  if(has_data) eg_url <- paste0(eg_url, "&hasData=1")
 
   eg_res <- httr::GET(eg_url)
 
